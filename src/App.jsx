91274c274868a29fb467b225f01a58914f3e1680
Foad <fwad40901@gmail.com>
2024-05-10 23:08:04 +0330

    Add daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Header from './components/Header';
+import Todos from './components/Todo/Todos';
+import Form from './components/Form';
 export default function App() {
   return (
-    <div>
-      <h1>Foad</h1>
+    <div className="h-screen flex flex-col items-center pt-40 gap-y-6">
+      <Header />
+      <Form />
+      <Todos />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Header() {
+  return <h1 className="Title text-5xl font-bold">Todo app</h1>;
+}
